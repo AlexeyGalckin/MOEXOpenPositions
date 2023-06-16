81dcd6fa2350e5bfd94c75825059939f733b76fb
Alexey <Alexe@DESKTOP-9LJ041A>
2023-06-16 23:21:04 +0300

    Init

---
+++ MOEXOpenPositions/MOEXOpenPositions/MOEXClient.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOEXOpenPositions
+{
+    interface IMOEXClient
+    {
+        void Update();
+        void Init(string symbol);
+        bool Get(DateTime time, out long value);
+        bool GetMinMax(out double min, out double max);
+    }
+    class MOEXClient : IMOEXClient
+    {
+        private const string _base = "https://iss.moex.com/iss/analyticalproducts/futoi/securities/{0}.json";
+        public bool Get(DateTime time, out long value)
+        {
+            throw new NotImplementedException();
+        }
+        public void Init(string symbol)
+        {
+            throw new NotImplementedException();
+        }
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+        public bool GetMinMax(out double min, out double max)
+        {
+            min = _min;
+            max = _max;
+            //
+            return true;
+        }
+        //
+        DateTime _last = DateTime.MinValue;
+        //
+        string _url;
+        Cookie _cookie;
+        //
+        long _min = long.MaxValue;
+        long _max = long.MinValue;
+        //
+        string _passport = "CWYRf4a4MYR1WzwdjEHKiQUAAAAIk2vp3llqix6hlne9tgCg8dspidbL5rGZgGkTM0HGD8X5_UMjHr-3s3l1nZSWZF1TAwdu1xpIiX2P28GdXg4X5dqx0vVZPcX6D3Cjvh_gNIpFdpUbpU8kUAvNf1i-aXH0zVRctDHR14eWQ71_JRkmtMIq7slboW1KQnm8wiFj-p30Ba4W0";
+    }
+}
---
+++ MOEXOpenPositions/MOEXOpenPositions/MOEXOpenPositions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TigerTrade.Chart.Indicators.Custom
+{
+    public class MOEXOpenPositions
+    {
+    }
+}
